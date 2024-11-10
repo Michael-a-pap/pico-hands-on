ffc63a4e245f5472bf32a9f7dd48d523db256015
Michael A. Pap. <michael_met@hotmail.com>
2024-11-10 12:39:33 +0200

    Cleaner simple_adc code

--- basics/adc_read/simple_adc.c
+++ basics/adc_read/simple_adc.c
@@ -5,20 +5,41 @@
 #include "hardware/gpio.h"
 #include "hardware/adc.h"
 
-#define ANALOG_PIN 26
+#define ANALOG_PIN0 0  // GPIO 26
+#define ANALOG_PIN1 1  // GPIO 27
+#define ANALOG_PIN2 2  // GPIO 28
+#define ANALOG_PIN3 3  // GPIO 29
+#define ANALOG_TEMP 4  //internal temperature ADC
+
 
 void setup() {
 	stdio_init_all();
 	adc_init();
-	adc_gpio_init(ANALOG_PIN);
-	adc_select_input(0); //0 means the 0th position of the analog multiplexer which corresponds to the 26GPIO
-	
+	adc_gpio_init(ANALOG_PIN0 + 26); //This makes sure that the pin is defined in a high-Z state
+	adc_set_temp_sensor_enabled (true);	
 }
+
+int readADC() {
+	adc_select_input(ANALOG_PIN0);
+
+	return adc_read();
+}
+
+int readTemperature() {
+	adc_select_input(ANALOG_TEMP);
+	uint16_t adc_value = adc_read();
+	
+	return (int)(27-((3.3/4096*adc_value)-0.706)/0.001721);
+
+}
+
 int main() {
 	setup();
+
 	while(1) {
-		uint16_t value = adc_read();
-		printf("The raw value of the adc is: %d\n", value);
+
+		printf("The raw value of the adc is: %d\n", readADC());
+		printf("The internal temperature is: %d\n", readTemperature());
 		sleep_ms(1000);
 	}
 }
